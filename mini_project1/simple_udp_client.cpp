108f771e7b4f2a7a0fe0bc5921089ed1847fcdb7
jamshann <jasha@DESKTOP-VAUIKA5.localdomain>
2018-04-11 09:55:12 -0600

    Almost there kinda

--- mini_project1/simple_udp_client.cpp
+++ mini_project1/simple_udp_client.cpp
@@ -9,7 +9,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
-
+#include <stdio.h>
 // #include "udpserver.h"
 // test
 
@@ -24,6 +24,32 @@
  * @param argv array of command line arguments
  * @return 0 on success, non-zero if an error occurred
  */
+struct ServerResponse{
+    uint16_t type;
+    uint16_t secret;
+  };
+  unsigned char hexval(unsigned char c)
+  {
+    if ('0' <= c && c <= '9')
+        return c - '0';
+    else if ('a' <= c && c <= 'f')
+        return c - 'a' + 10;
+    else if ('A' <= c && c <= 'F')
+        return c - 'A' + 10;
+    else abort();
+  }
+  std::string HEX2STR (std::string str)
+{
+    std::string tmp;
+    const char *c = str.c_str();
+    unsigned int x;
+    while(*c != 0) {
+        sscanf(c, "%2X", &x);
+        tmp += x;
+        c += 2;
+    }
+    return tmp;
+  }
 int main(int argc, char *argv[]) {
   // Alias for argv[1] for convenience
   char *ip_string;
@@ -39,12 +65,17 @@ int main(int argc, char *argv[]) {
   int ret;
   // IPv4 structure representing and IP address and port of the destination
   struct sockaddr_in dest_addr;
+  // IPv$ structure representing the IP address and port of responding server
+  struct sockaddr_in server_addr;
+  // Holds the length of the server ip address
+  socklen_t server_addr_length;
+  // Buffer for the bytes returned from a server
+  uint16_t recvfrom_buff[2048];
+  // server response structure
+  struct ServerResponse server_response;
 
-  // Set dest_addr to all zeroes, just to make sure it's not filled with junk
-  // Note we could also make it a static variable, which will be zeroed before execution
   memset(&dest_addr, 0, sizeof(struct sockaddr_in));
 
-  // Note: this needs to be 4, because the program name counts as an argument!
   if (argc < 4) {
     std::cerr << "Please specify IP PORT DATA as first three arguments." << std::endl;
     return 1;
@@ -54,25 +85,16 @@ int main(int argc, char *argv[]) {
   port_string = argv[2];
   data_string = argv[3];
 
-  // Create the UDP socket.
-  // AF_INET is the address family used for IPv4 addresses
-  // SOCK_DGRAM indicates creation of a UDP socket
   udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
-  // Make sure socket was created successfully, or exit.
   if (udp_socket == -1) {
     std::cerr << "Failed to create udp socket!" << std::endl;
     std::cerr << strerror(errno) << std::endl;
     return 1;
   }
 
-  // inet_pton converts an ip address string (e.g., 1.2.3.4) into the 4 byte
-  // equivalent required for using the address in code.
-  // Note that because dest_addr is a sockaddr_in (again, IPv4) the 'sin_addr'
-  // member of the struct is used for the IP
-  ret = inet_pton(AF_INET, ip_string, (void *)&dest_addr.sin_addr);
 
-  // Check whether the specified IP was parsed properly. If not, exit.
+  ret = inet_pton(AF_INET, ip_string, (void *)&dest_addr.sin_addr);
   if (ret == -1) {
     std::cerr << "Failed to parse IPv4 address!" << std::endl;
     std::cerr << strerror(errno) << std::endl;
@@ -80,10 +102,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Convert the port string into an unsigned integer.
   ret = sscanf(port_string, "%u", &port);
-  // sscanf is called with one argument to convert, so the result should be 1
-  // If not, exit.
   if (ret != 1) {
     std::cerr << "Failed to parse port!" << std::endl;
     std::cerr << strerror(errno) << std::endl;
@@ -91,21 +110,11 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Set the address family to AF_INET (IPv4)
   dest_addr.sin_family = AF_INET;
-  // Set the destination port. Use htons (host to network short)
-  // to ensure that the port is in big endian format
   dest_addr.sin_port = htons(port);
-
-  // Send the data to the destination.
-  // Note 1: we are sending strlen(data_string) + 1 to include the null terminator
-  // Note 2: we are casting dest_addr to a struct sockaddr because sendto uses the size
-  //         and family to determine what type of address it is.
-  // Note 3: the return value of sendto is the number of bytes sent
   ret = sendto(udp_socket, data_string, strlen(data_string) + 1, 0,
                (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in));
 
-  // Check if send worked, clean up and exit if not.
   if (ret == -1) {
     std::cerr << "Failed to send data!" << std::endl;
     std::cerr << strerror(errno) << std::endl;
@@ -115,10 +124,96 @@ int main(int argc, char *argv[]) {
 
   std::cout << "Sent " << ret << " bytes out." << std::endl;
 
-  /**
-   * Code to receive response from the server goes here!
-   * recv or recvfrom...
-   */
+  server_addr_length = sizeof(struct sockaddr_in);
+
+  ret = recvfrom(udp_socket, &recvfrom_buff, 2047, 0, (struct sockaddr *)&server_addr, &server_addr_length); // Receive up to 2048 bytes of data
+
+  // If less than 4 bytes were returned then the server struct failed to send or some other error occurred
+  if (ret < 4) {
+    std::cerr << "Failed to recvfrom!" << std::endl;
+    std::cerr << strerror(errno) << std::endl;
+    close(udp_socket);
+    return 1;
+  }
+  std::cout << "Received " << ret << " bytes from server \n";
+
+
+  uint16_t response_type = recvfrom_buff[0]; // ServerResponse.type based on the header file
+
+  enum ResponseType{Echo = 0, Empty = 1,  Error = 2};
+  switch(response_type){
+    case Empty :
+              std::cout << "Received EMPTY response from server." << std::endl;
+              break;
+    case Echo :
+              std::cout << "Received ECHO response from server." << std::endl;
+              break;
+    case Error :
+              std::cout << "Received ERROR response from server." << std::endl;
+              break;
+    default: std::cout << "Response type error." << std::endl;
+              return 1;
+  }
+  std::cout << "Secret was " << recvfrom_buff[1] << std::endl; // ServerResponse.secret based on header file
+  char first_byte;
+  char second_byte;
+  std::string temp_string;
+  std::string string_response;
+
+  int n;
+  if (response_type = 0){
+    for (int i = 2; i < ret; i++){
+      first_byte = (uint8_t)recvfrom_buff[i] >> 8; // Bit shift to get the 'higher' byte
+      second_byte = (uint8_t)recvfrom_buff[i] & 0xff; // Use a mask to get 'lower byte'
+    //  first_byte = hexval(first_byte);
+      //second_byte = hexval(second_byte);
+      //first_byte = (first_byte << 4) + first_byte;
+      //second_byte = (second_byte << 4) + second_byte;
+      string_response += std::to_string(first_byte);
+      string_response += std::to_string(second_byte);
+      //temp_string += std::to_string(first_byte);
+      //temp_string += std::to_string(second_byte);
+      //std::string byte = temp_string.substr(i,2);
+      //char chr = (char) (int)strtol(byte.c_str(), NULL, 16);
+      //string_response.push_back(chr);
+    //std::cout << (char)first_byte[0];
+    //std::cout << (char)second_byte[0];
+    //sprintf(string_response, "%c%c", first_byte[0], second_byte[0]); // += std::to_string((char)first_byte[0]);
+    //sprintf(string_response, "%c", second_byte[0]); //string_response += std::to_string((char)second_byte[0]);
+    //printf("%c%c", first_byte, second_byte);
+  }
+   //printf("%s\n", string_response); //std::cout << string_response << std::endl;
+  //temp_string = HEX2STR(string_response);
+   std::cout << string_response << std::endl;
+}
+
+
+if (response_type = 1){
+  for (int i = 2; i < ret; i++){
+    first_byte = (uint8_t)recvfrom_buff[i] >> 8; // Bit shift to get the 'higher' byte
+    second_byte = (uint8_t)recvfrom_buff[i] & 0xff; // Use a mask to get 'lower byte'
+
+    //first_byte = (first_byte << 4) + first_byte;
+  //  second_byte = (second_byte << 4) + second_byte;
+    string_response += std::to_string(first_byte);
+    string_response += std::to_string(second_byte);
+    //temp_string += std::to_string(first_byte);
+    //temp_string += std::to_string(second_byte);
+    //std::string byte = temp_string.substr(i,2);
+    //char chr = (char) (int)strtol(byte.c_str(), NULL, 16);
+    //string_response.push_back(chr);
+    //std::cout << (char)first_byte[0];
+    //std::cout << (char)second_byte[0];
+    //sprintf(string_response, "%c%c", first_byte[0], second_byte[0]); // += std::to_string((char)first_byte[0]);
+    //sprintf(string_response, "%c", second_byte[0]); //string_response += std::to_string((char)second_byte[0]);
+    //printf("%c%c", first_byte, second_byte);
+    }
+    //printf("%s\n", string_response); //std::cout << string_response << std::endl;
+    //std::cout << string_response << std::endl;
+  //  temp_string = HEX2STR(string_response);
+     std::cout << string_response << std::endl;
+  }
+
 
   close(udp_socket);
   return 0;
