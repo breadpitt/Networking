3b9d71e2356e718dbbbf530027cd8ee9adc2014f
jamshann <jashannon10@gmail.com>
2018-05-06 20:16:54 -0600

    mmmmmm

--- CompNet_Project1/sucms_list.cpp
+++ CompNet_Project1/sucms_list.cpp
@@ -29,43 +29,6 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
-std::vector<string> parseFile(string &filename) {
-
-	string line;
-	line.clear();
-	ifstream pwFile;
-	pwFile.open(filename.c_str());
-	if (!pwFile.is_open()) {
-		cerr << "ERROR: failed to open trace file: " << filename << "\n";
-		exit(2);
-	}
-	vector<string> loginString;
-	
-	int i = 0;
-	// Read next line
-	if (std::getline(pwFile, line)) {
-
-		// Make a string stream from command line
-		istringstream ss(line);
-		string token;
-
-		while (getline(ss, token, ',')) {
-			ss.ignore();
-			loginString.push_back(token);
-			cout << loginString[i] << std::endl;
-			i++;
-		}
-		return loginString;
-	}
-	else if (pwFile.eof()) {
-    cout << "Empty file error\n";
-		return loginString;
-	}
-	else {
-		cerr << "ERROR: getline failed for" << filename << "\n";
-		exit(2);
-	}
-}
 /**
  *
  * Dead simple UDP client example. Reads in IP PORT DATA
@@ -89,12 +52,13 @@ int main(int argc, char *argv[]) {
   // Variable used to check return codes from various functions
   int ret;
   // IPv4 structure representing and IP address and port of the destination
-  struct sockaddr_in dest_addr;
-  // IPv4 structure representing the IP address and port of responding server
   struct sockaddr_in server_addr;
+  // IPv4 structure representing the IP address and port of responding server
+  //struct sockaddr_in server_addr;
+  struct sockaddr from;
   // Holds the length of the server ip address
-  socklen_t server_addr_length;
-  server_addr_length = sizeof(struct sockaddr_in);
+  socklen_t from_addr_length;
+  from_addr_length = sizeof(struct sockaddr);
   // Variable used to store a user's name
   string username;
   // Variable used to store a user's password
@@ -102,9 +66,9 @@ int main(int argc, char *argv[]) {
   // Variable used to store a user's permissions
   string permissions;
 
-  // Set dest_addr to all zeroes, just to make sure it's not filled with junk
+  // Set server_addr to all zeroes, just to make sure it's not filled with junk
   // Note we could also make it a static variable, which will be zeroed before execution
- // memset(&dest_addr, 0, sizeof(struct sockaddr_in));
+  memset(&server_addr, 0, sizeof(struct sockaddr_in));
 
   // Note: this needs to be 4, because the program name counts as an argument!
   if (argc < 3) { // change back to 4 after hard code testing
@@ -128,7 +92,45 @@ int main(int argc, char *argv[]) {
     std::cin >> password;
     //permissions = "rwd";
   
-  CommandMessage initList; // Create the command message
+  
+  // Create the UDP socket. AF_INET used for IPv4 addresses. SOCK_DGRAM indicates creation of a UDP socket
+  udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+
+  // Make sure socket was created successfully, or exit.
+  if (udp_socket == -1) {
+    std::cerr << "Failed to create udp socket!" << std::endl;
+    return 1;
+  }
+
+  // inet_pton converts an ip addr str into the required format
+  // Note that because server_addr is a sockaddr_in (again, IPv4) the 'sin_addr'
+  // member of the struct is used for the IP
+  ret = inet_pton(AF_INET, ip_string, (void *)&server_addr.sin_addr);
+
+  // Check whether the specified IP was parsed properly. If not, exit.
+  if (ret == -1) {
+    std::cerr << "Failed to parse IPv4 address!" << std::endl;
+    close(udp_socket);
+    return 1;
+  }
+
+  // Convert the port string into an unsigned integer.
+  ret = sscanf(port_string, "%u", &port);
+  // sscanf is called with one argument to convert, so the result should be 1
+  if (ret != 1) {
+    std::cerr << "Failed to parse port!" << std::endl;
+    close(udp_socket);
+    return 1;
+  }
+
+  // Set the address family to AF_INET (IPv4)
+  server_addr.sin_family = AF_INET;
+  // Set the destination port. Use htons (host to network short)
+  // to ensure that the port is in big endian format
+  server_addr.sin_port = htons(port);
+
+
+CommandMessage initList; // Create the command message
   initList.username_len = strlen(username.c_str());
   initList.command = 80; // 80 is LIST
   
@@ -157,65 +159,19 @@ int main(int argc, char *argv[]) {
     initVector.push_back(convertVec[i]);
   }
   
-  /*
-  memcpy(initBuf, (const void *)&initHeader, sizeof(initHeader));
-  std::cout << "Size of headersize: " <<  initHeaderSize << "\n";
-  std::cout << "Size of listsize: " <<  initListSize << "\n";
-  memcpy(initBuf + initHeaderSize, (const void *)&initList, sizeof(initList));
-   std::cout << "Size of bufsize: " <<  initBufSize << "\n";
-  memcpy(initBuf + initHeaderSize + initListSize, username.c_str(), initList.username_len);
-  */
+  
   for (int i = 0; i <initVector.size(); i++){
     printf(" %u ", (unsigned int)initVector[i] );
 
   }
-  // Create the UDP socket.
-  // AF_INET is the address family used for IPv4 addresses
-  // SOCK_DGRAM indicates creation of a UDP socket
-  udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-
-  // Make sure socket was created successfully, or exit.
-  if (udp_socket == -1) {
-    std::cerr << "Failed to create udp socket!" << std::endl;
-    return 1;
-  }
-
-  // inet_pton converts an ip address string (e.g., 1.2.3.4) into the 4 byte
-  // equivalent required for using the address in code.
-  // Note that because dest_addr is a sockaddr_in (again, IPv4) the 'sin_addr'
-  // member of the struct is used for the IP
-  ret = inet_pton(AF_INET, ip_string, (void *)&dest_addr.sin_addr);
-
-  // Check whether the specified IP was parsed properly. If not, exit.
-  if (ret == -1) {
-    std::cerr << "Failed to parse IPv4 address!" << std::endl;
-    close(udp_socket);
-    return 1;
-  }
-
-  // Convert the port string into an unsigned integer.
-  ret = sscanf(port_string, "%u", &port);
-  // sscanf is called with one argument to convert, so the result should be 1
-  // If not, exit.
-  if (ret != 1) {
-    std::cerr << "Failed to parse port!" << std::endl;
-    close(udp_socket);
-    return 1;
-  }
-
-  // Set the address family to AF_INET (IPv4)
-  dest_addr.sin_family = AF_INET;
-  // Set the destination port. Use htons (host to network short)
-  // to ensure that the port is in big endian format
-  dest_addr.sin_port = htons(port);
 
   // Send the data to the destination.
   // Note 1: we are sending strlen(data_string) + 1 to include the null terminator
-  // Note 2: we are casting dest_addr to a struct sockaddr because sendto uses the size
+  // Note 2: we are casting server_addr to a struct sockaddr because sendto uses the size
   //         and family to determine what type of address it is.
   // Note 3: the return value of sendto is the number of bytes sent
- ret = sendto(udp_socket, &initVector[0], initVector.size(), 0,
-               (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in));
+ ret = sendto(udp_socket, &initVector[0], sizeof(initVector[0]) * initVector.size(), 0,
+               (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
 
   // Check if send worked, clean up and exit if not.
   if (ret == -1) {
@@ -228,11 +184,13 @@ int main(int argc, char *argv[]) {
 
   // The header & response should only be about 7 uint_16s but I'm adding some buffer to my buffer
   //  just in case I messed up my counting, which is always possible
-  uint16_t recvfrom_buf[32];
+  uint16_t recv_buf[1024];
   uint16_t messageType, messageLength; // sucms message type and length
   uint16_t commandCode, resultID, messageCount; // command response variables
-  uint32_t messageDataSize; // Size of data received by command response 
-  ret = recvfrom(udp_socket, &recvfrom_buf, 31, 0, (struct sockaddr *)&server_addr, &server_addr_length); // Receive up to 64 bytes of data
+  uint32_t messageDataSize; // Size of data received by command response
+  //int from_addr_length;
+  //from_addr_length = sizeof(from); 
+  ret = recvfrom(udp_socket, &recv_buf, sizeof(recv_buf) - 1, 0, &from, &from_addr_length); // Receive up to 1024 bytes of data
 
   if (ret < 4) {
     std::cerr << "Failed to recvfrom!" << std::endl;
@@ -240,18 +198,20 @@ int main(int argc, char *argv[]) {
     close(udp_socket);
     return 1;
   }
-
-  messageType = ntohs(recvfrom_buf[0]);
+  recv_buf[ret] = '\0';
+  
+  std::cout << "Received " << ret << " bytes." << std::endl;
+  messageType = ntohs(recv_buf[0]);
   std::cout << "messageType: " << messageType << "\n";
-  messageLength = ntohs(recvfrom_buf[1]);
+  messageLength = ntohs(recv_buf[1]);
   std::cout << "messageLength: " << messageLength << "\n";
-  commandCode = ntohs(recvfrom_buf[2]);
+  commandCode = ntohs(recv_buf[2]);
   std::cout << "commandCode: " << commandCode << "\n";
-  resultID = ntohs(recvfrom_buf[3]);
+  resultID = ntohs(recv_buf[3]);
   // 32 bit variable likely got chopped up into two 16 bit slots so we need to cast, shift, and cat in order to get it back into the right format
-  messageDataSize = (ntohs((uint32_t)recvfrom_buf[4]) << 16) | ntohs(recvfrom_buf[5]);
+  messageDataSize = (ntohs((uint32_t)recv_buf[4]) << 16) | ntohs(recv_buf[5]);
   std::cout << "messageDataSize: " << messageDataSize << "\n";
-  messageCount = (ntohs(recvfrom_buf[6])); 
+  messageCount = (ntohs(recv_buf[6])); 
   std::cout << "messageCount: " << messageCount << "\n";
 
 
